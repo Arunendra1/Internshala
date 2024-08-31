9c9e4c1b0feabc5dc77bade0eec5fd01e45f0edb
Arunendra Bhardwaj <mutursingh1111@gmail.com>
2024-08-31 15:00:34 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">unfluke</div>
+      <div className="navbar-brand">fluke</div>
       <ul className="navbar-nav">
         <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
         <li className="nav-item"><a href="#leaderboard" className="nav-link">Leaderboard</a></li>
